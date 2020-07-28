f66566263b219e02ff96d04de0aafb483b477aa5
Thompsonmaxdwell <oghenefokethompson@gmail.com>
2020-07-28 00:41:40 +0000

    Done with slide

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,42 +1,44 @@
-let carousel_ul = document.querySelector('.slider_ul_tracker');
-let carousel_li =  carousel_ul.children;
-let carousel_width = carousel_li[0].getBoundingClientRect().width;
-let slide_right_btn = document.querySelector('.slide_right_btn');
-let slide_left_btn = document.querySelector('.slide_left_btn');
+let slide_container = document.querySelector('.slide_container');
+let slides = slide_container.children;
+let slide_width = slides[0].getBoundingClientRect().width;
+let next_slide = document.querySelector('.next_slide');
+let prev_slide = document.querySelector('.prev_slide');
+let slide_counter = 1;
 
+slide_container.style.transform = 'translateX(' + -slide_width * slide_counter + 'px' + ')';
 
-// Arranging  The  Element in the Slide
-Array.from(carousel_li).forEach((item, index) => {
-    item.style.left = carousel_width * index + 'px';
-    
-});
+//   Next  Slide Event tigger
+const next_Slide_event = (e) => {
+	if (slide_counter >= slides.length - 1) return;
+	slide_container.style.transition = 'transform 0.7s ease-in-out';
+	slide_counter++;
+	slide_container.style.transform = 'translateX(' + -slide_width * slide_counter + 'px' + ')';
+};
+//   previous Slide Event tigger
+const prev_Slide_Event = (e) => {
+	if (slide_counter <= 0) return;
+	slide_container.style.transition = 'transform 0.7s ease-in-out';
+	slide_counter--;
+	slide_container.style.transform = 'translateX(' + -slide_width * slide_counter + 'px' + ')';
+};
 
-const mySlider = (carousel_ul,  targetSlide, nextSlide)=>{
-    carousel_ul.style.transform ='translate(-'+ nextSlide.style.left  +')'
-    targetSlide.classList.remove('current_slider');
-    nextSlide.classList.add('current_slider');
+//     tigger when transition finish
+const slide_container_Event = (e) => {
+	if (slides[slide_counter].id === 'last_slide') {
+		slide_container.style.transition = 'none';
+		console.log(slide_counter);
+		slide_counter = slides.length - 2;
+		slide_container.style.transform = 'translateX(' + -slide_width * slide_counter + 'px' + ')';
+	}
 
-}
+	if (slides[slide_counter].id === 'first_slide') {
+        slide_container.style.transition = 'none';
+		slide_counter = slides.length - slide_counter;
+		slide_container.style.transform = 'translateX(' + -slide_width * slide_counter + 'px' + ')';
+	}
+};
 
-
-slide_right_btn.addEventListener('click', (e)=>{
-    let targetSlide = carousel_ul.querySelector('.current_slider');
-    let nextSlide = targetSlide.nextElementSibling;
-    
-    if(nextSlide == null){
-      console.log(nextSlide)
-        return
-     }
-    mySlider(carousel_ul, targetSlide, nextSlide)
-    
-
-})
-
-
-slide_left_btn.addEventListener('click', (e)=>{
-    let targetSlide = carousel_ul.querySelector('.current_slider');
-    let nextSlide = targetSlide.previousElementSibling;
-    mySlider(carousel_ul, targetSlide, nextSlide);
-    
-
-})
+next_slide.addEventListener('click', next_Slide_event);
+prev_slide.addEventListener('click', prev_Slide_Event);
+slide_container.addEventListener('transitionend', slide_container_Event);
+setInterval(next_Slide_event, 4000);
