ecf72739cc4cb988de92bdf0f8c4036110c621d5
Thompsonmaxdwell <oghenefokethompson@gmail.com>
2020-08-06 21:36:25 +0000

    Fixed Navbar Correct fixed

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,24 @@
-let bars = document.querySelectorAll('#bars');
 
+// let nav = document.querySelector('#nav');
+let bars = document.querySelectorAll('#bars');
 bars.forEach(bar =>{
-  bar.addEventListener('click', (e)=>{
-   let nav =  bar.parentElement.previousElementSibling.querySelector('#nav')
-    bar.classList.toggle('change');
   
-    nav.className == 'header_close'? nav.classList.add('header_open') :  nav.classList.remove('header_open');
-  });
+   bar.addEventListener('click', (e)=>{
+      let header_right = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector('#header_right');
+      let nav = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector('#nav')
+      bar.classList.toggle('change');
+     console.log(nav)
+      if(nav.className == 'nav_close'){
+        nav.classList.add('nav_open');
+        header_right.classList.add('header_right_open');
   
-})
+      }else {
+        nav.classList.remove('nav_open');
+        header_right.classList.remove('header_right_open');
+  
+      }
+   
+    });
+    
+
+  })
